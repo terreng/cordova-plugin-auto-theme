2c378e058dcc4cd4e79b3735b9f5ab190c7d7a03
Terren <11605395+terreng@users.noreply.github.com>
2020-11-07 14:29:38 -0800

    Update AutoTheme.java

--- src/android/AutoTheme.java
+++ src/android/AutoTheme.java
@@ -10,14 +10,21 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.os.Process;
 
+import android.content.res.Configuration;
+
 public class AutoTheme extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
             
         if (action.equals("getTheme")) {
-			
-            callbackContext.success(1);
+		
+		int uiMode = cordova.getActivity().getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+		if (uiMode == Configuration.UI_MODE_NIGHT_YES) {
+			callbackContext.success("true");
+		} else {
+			callbackContext.success("false");
+		}
 
             return true;
 
